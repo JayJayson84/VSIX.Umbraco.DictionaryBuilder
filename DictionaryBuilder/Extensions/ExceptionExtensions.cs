3989ac89967c2326b23fad4e1956a8db09c053c4
JayJayson84 <jayjayson84@bitbucket.org>
2021-03-28 19:20:11 +0100

    Adds an extension method to flatten aggregate exceptions into a single error message.

---
+++ DictionaryBuilder/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace DictionaryBuilder.Extensions
+{
+    public static class Exceptions
+    {
+
+        /// <summary>
+        /// Flattens one or more aggregate exceptions into a single error message.
+        /// </summary>
+        /// <param name="aggregateExeption">Represents one or more errors that occur during application execution.</param>
+        /// <returns>The error message that explains the reason for the aggregated exceptions.</returns>
+        public static string AggregateMessage(this AggregateException aggregateExeption)
+        {
+            var aggregateExceptions = aggregateExeption.Flatten().InnerExceptions;
+            
+            var sb = new StringBuilder($"DictionaryBuilder: ({aggregateExceptions.Count}) errors occurred:")
+                .AppendLine()
+                .AppendLine();
+
+            for (var i = 0; i < aggregateExceptions.Count; i++)
+            {
+                sb.AppendLine($"[Error {i + 1}]")
+                  .AppendLine($"{aggregateExceptions[i].GetType()}: {aggregateExceptions[i].Message}")
+                  .AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
